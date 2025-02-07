8a6963fb51008fa56c6cd5d0a12544eb46afc552
Yasmin Mejia <yasmejia052@gmail.com>
2025-02-07 10:40:02 -0600

    holiiii

--- codigo.java
+++
@@ -1,4 +0,0 @@
-document.getElementById("colorButton").addEventListener("click", function() {
-    const colors = ["red", "green", "blue", "yellow", "purple", "orange"];
-    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-});
